5d6b98f52c835b22f0b6699a34ce811a2f4b58ca
jk <jk2@gmail.com>
2023-08-17 03:23:09 -0700

    p3

---
+++ four.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+arr=np.array([[1,2,3],
+            [4,5,6]])
+print(arr+1)
+print(" ")
+print(arr-1)
+print(" ")
+print(arr*3)
+print(" ")
+print(arr/2)
+print(" ")
+print(arr**3)
+print(" ")
+print(arr.flatten())
